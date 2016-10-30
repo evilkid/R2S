9419112c7fea5124443db7bd074910a159f1a4ba
Ouerghi Yassine <yassine.ouerghi@esprit.tn>
2016-10-30 19:22:40 +0100

    HS-84  As a Recruitment Manager, I want to list all the variables i can use in the email model

--- tn.esprit.R2S-web/src/main/java/tn/esprit/R2S/resource/EmailModelResource.java
+++ tn.esprit.R2S-web/src/main/java/tn/esprit/R2S/resource/EmailModelResource.java
@@ -1,22 +1,21 @@
 package tn.esprit.R2S.resource;
 
 import tn.esprit.R2S.interfaces.IEmailModelService;
-import tn.esprit.R2S.model.Candidate;
+import tn.esprit.R2S.model.Address;
 import tn.esprit.R2S.model.EmailModel;
+import tn.esprit.R2S.model.Job;
+import tn.esprit.R2S.model.Users;
 
 import javax.ejb.EJB;
 import javax.json.Json;
-import javax.json.JsonArrayBuilder;
 import javax.json.JsonObjectBuilder;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.lang.reflect.Field;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 @Path("/api/email-model")
 //@Secured(Roles.RECRUITMENT_MANAGER)
@@ -73,54 +72,47 @@ public class EmailModelResource {
         //TODO: investigate more
         JsonObjectBuilder variables = Json.createObjectBuilder();
 
+        Field[] user = Users.class.getDeclaredFields();
+        Field[] address = Address.class.getDeclaredFields();
+        Field[] job = Job.class.getDeclaredFields();
 
-        //if (Candidate.class.getSuperclass() != null) {
-
-        JsonArrayBuilder inner = Json.createArrayBuilder();
-
-        Field[] fields = Candidate.class.getDeclaredFields();
-        if (Candidate.class.getSuperclass() != null) {
-            Field[] superFields = Candidate.class.getSuperclass().getDeclaredFields();
-
-            fields = Stream.concat(Arrays.stream(fields), Arrays.stream(superFields))
-                    .toArray(Field[]::new);
-        }
-
-
-        System.out.println(Arrays.toString(fields));
-
-
-        for (Field field : fields) {
-
-            if (field.getType().getPackage().getName().startsWith("tn.esprit.R2S.model")) {
-                JsonArrayBuilder innerArray = Json.createArrayBuilder();
-                for (Field innerField : field.getType().getDeclaredFields()) {
-                    innerArray.add(innerField.getName());
-                }
-
-                inner.add(
-                        Json.createObjectBuilder()
-                                .add(field.getName(), innerArray)
-                );
-
-            } else {
-                inner.add(field.getName());
+        //candidate fields
+        JsonObjectBuilder candidateObjectBuilder = Json.createObjectBuilder();
+        for (Field field : user) {
+            if (!field.getName().contains("address")) {
+                candidateObjectBuilder.add(field.getName(), "{{candidate." + field.getName() + "}}");
             }
-
-
-            //System.out.println(field.getType());
-        }
-        variables.add("candidate", inner);
-        //}
-
-/*
-        JsonArrayBuilder inner = Json.createArrayBuilder();
-        for (Field field : Candidate.class.getDeclaredFields()) {
-
-            inner.add(field.getName());
         }
 
-        variables.add("candidate", inner);*/
+        JsonObjectBuilder addressObjectBuilder = Json.createObjectBuilder();
+        for (Field field : address) {
+            addressObjectBuilder.add(field.getName(), "{{candidate.address." + field.getName() + "}}");
+        }
+
+        candidateObjectBuilder.add("Address", addressObjectBuilder.build());
+
+        variables.add("Candidate", candidateObjectBuilder.build());
+
+        //referee fields
+        JsonObjectBuilder refereeObjectBuilder = Json.createObjectBuilder();
+        for (Field field : user) {
+            if (!field.getName().contains("address")) {
+                refereeObjectBuilder.add(field.getName(), "{{referee." + field.getName() + "}}");
+            }
+        }
+
+        variables.add("Referee", refereeObjectBuilder.build());
+
+        //job fields
+        JsonObjectBuilder jobObjectBuilder = Json.createObjectBuilder();
+        for (Field field : job) {
+            System.out.println(field + ", " + field.getType().getPackage().getName());
+            if (field.getType() != List.class) {
+                jobObjectBuilder.add(field.getName(), "{{job." + field.getName() + "}}");
+            }
+        }
+
+        variables.add("Job", jobObjectBuilder.build());
         return Response.ok(variables.build()).build();
     }
 }
