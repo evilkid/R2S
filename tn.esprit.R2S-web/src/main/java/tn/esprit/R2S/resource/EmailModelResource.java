00c10a3141cf5fb51cb417a35ef658b4925fe4e0
Ouerghi Yassine <yassine.ouerghi@esprit.tn>
2016-10-30 12:09:19 +0100

    reflection API baby steps

--- tn.esprit.R2S-web/src/main/java/tn/esprit/R2S/resource/EmailModelResource.java
+++ tn.esprit.R2S-web/src/main/java/tn/esprit/R2S/resource/EmailModelResource.java
@@ -1,20 +1,25 @@
 package tn.esprit.R2S.resource;
 
 import tn.esprit.R2S.interfaces.IEmailModelService;
+import tn.esprit.R2S.model.Candidate;
 import tn.esprit.R2S.model.EmailModel;
-import tn.esprit.R2S.resource.util.Roles;
-import tn.esprit.R2S.resource.util.Secured;
 
 import javax.ejb.EJB;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import java.lang.reflect.Field;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 @Path("/api/email-model")
-@Secured(Roles.RECRUITMENT_MANAGER)
+//@Secured(Roles.RECRUITMENT_MANAGER)
 public class EmailModelResource {
 
 
@@ -61,4 +66,61 @@ public class EmailModelResource {
                 }).orElseThrow(NotFoundException::new);
     }
 
+    @Path("/variables")
+    @GET
+    public Response getVariables() {
+
+        //TODO: investigate more
+        JsonObjectBuilder variables = Json.createObjectBuilder();
+
+
+        //if (Candidate.class.getSuperclass() != null) {
+
+        JsonArrayBuilder inner = Json.createArrayBuilder();
+
+        Field[] fields = Candidate.class.getDeclaredFields();
+        if (Candidate.class.getSuperclass() != null) {
+            Field[] superFields = Candidate.class.getSuperclass().getDeclaredFields();
+
+            fields = Stream.concat(Arrays.stream(fields), Arrays.stream(superFields))
+                    .toArray(Field[]::new);
+        }
+
+
+        System.out.println(Arrays.toString(fields));
+
+
+        for (Field field : fields) {
+
+            if (field.getType().getPackage().getName().startsWith("tn.esprit.R2S.model")) {
+                JsonArrayBuilder innerArray = Json.createArrayBuilder();
+                for (Field innerField : field.getType().getDeclaredFields()) {
+                    innerArray.add(innerField.getName());
+                }
+
+                inner.add(
+                        Json.createObjectBuilder()
+                                .add(field.getName(), innerArray)
+                );
+
+            } else {
+                inner.add(field.getName());
+            }
+
+
+            //System.out.println(field.getType());
+        }
+        variables.add("candidate", inner);
+        //}
+
+/*
+        JsonArrayBuilder inner = Json.createArrayBuilder();
+        for (Field field : Candidate.class.getDeclaredFields()) {
+
+            inner.add(field.getName());
+        }
+
+        variables.add("candidate", inner);*/
+        return Response.ok(variables.build()).build();
+    }
 }
